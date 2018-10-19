1dea54a5700016bc6e6660aaafa0a4ddf7ede843
Hedge77 <42855670+Hedge77@users.noreply.github.com>
2018-10-19 16:35:42 -0500

    lab 2

--- mfh/pwshAzure.ps1
+++ mfh/pwshAzure.ps1
@@ -12,3 +12,22 @@ import-module Azure
 Import-Module "C:\Program Files (x86)\Microsoft SDKs\Azure\PowerShell\ServiceManagement\Azure\Azure.psd1"
 # Login in to Azure
 Login-AzureRMAccount
+
+#test script
+$rg="lab1"
+$job="job3"
+$sa="mfhsa0001"
+$template="azuredeploy.json"
+New-AzureRMResourceGroupDeployment -name $job -storageaccount $sa -templateFile $template -ResourceGroup $rg
+
+#testing template validity
+Test-AzureRMResourceGroupDeployment -name $job -storageaccount $sa -templateFile $template -ResourceGroup $rg
+New-AzureRMResourceGroupDeployment -TemplateFile .\azuredeploy.json -ResourceGroupName $rg -Name $job
+
+#new
+$rg="lab1"
+$job='job.'+((Get-Date).ToUniversalTime()).ToString("MMddyy.HHmm")
+$template="azuredeploy.json"
+$saPrefix = "mfhsa"
+$storageAccount = (New-AzureRMResourceGroupDeployment -Name $job -StorageAccountPrefix $saPrefix -TemplateFile $template -ResourceGroupName $rg ).Output
+echo $storageAccount
